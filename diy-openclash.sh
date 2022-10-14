20add37b95f18e7a61397205f6171b697be78a0f
zelotoj <zelotoj@gmail.com>
2022-10-14 13:49:29 +0800

    Update diy-openclash.sh

--- diy-openclash.sh
+++ diy-openclash.sh
@@ -1,18 +1,4 @@
-#wget https://github.com/vernesong/OpenClash/archive/master.zip
-#unzip master.zip
-#cp -r OpenClash-master/luci-app-openclash package
-#rm -rf OpenClash-master master.zip
-
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
-
-# 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+unzip master.zip
+cp -r OpenClash-master/luci-app-openclash package
+rm -rf OpenClash-master master.zip
