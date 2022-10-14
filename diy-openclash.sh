9eac311c8de0f262bea5bc60c26e3a14cefdd577
zelotoj <zelotoj@gmail.com>
2022-10-14 09:44:05 +0800

    Update diy-openclash.sh

--- diy-openclash.sh
+++ diy-openclash.sh
@@ -1,4 +1,4 @@
 wget https://github.com/vernesong/OpenClash/archive/master.zip
 unzip master.zip
-cp -r OpenClash-master/luci-app-openclash openwrt/package
+cp -r OpenClash-master/luci-app-openclash package
 rm -rf OpenClash-master
